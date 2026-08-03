bb8128c6c0655037503ca285f306451780cf8bbf
DESKTOP-KDALOQ5\Sandu <vejitta123@gmail.com>
2026-08-03 12:15:37 +0300

    Initial Spring Boot project

---
+++ src/main/java/com/skillforge/skillforge_monolith/SkillforgeMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.skillforge.skillforge_monolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillforgeMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillforgeMonolithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillforge/skillforge_monolith/SkillforgeMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillforge.skillforge_monolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillforgeMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
